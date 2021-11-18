2cfe3132b27968ce01a1a9fd35bbb58065ff2c65
Willian Matheus Ribeiro Tesche <85637800+williamtesche@users.noreply.github.com>
2021-11-18 15:33:59 -0300

    terminado

--- src/App.js
+++ src/App.js
@@ -1,37 +1,40 @@
 import { useState } from "react";
+import './index.css'
 
-export default function Contador({nome}) {
 
-  const [numero,setNumero] = useState(0)
+export default function Contador() {
 
-  function mais() {
-    setNumero(numero + 1)
+  const [numero, setNumero] = useState(0)
+
+  function mais(){
+    setNumero(numero  + 1)
   }
 
-
-
-  function menos() {
-    setNumero(numero - 1)
-  }
-
-
-
-  function reset() {
+  function reset(){
     setNumero(0)
   }
+  
+  function menos(){
+    setNumero(numero  - 1)
+  }
 
 
+  
 
 
-  return (
-    <>
-     <h1>{nome}</h1>
-     <p>{numero}</p>
+  return <div>
+      <h1>contador</h1>
+      <p>{numero}</p>
+      <button onClick={mais}>mais</button>
+      <button onClick={reset}>reset</button>
+      <button onClick={menos}>menos</button>
+      
+  </div>
 
-     <button onclick={mais}>mais</button>
-     <button onclick={reset}>reset</button>
-     <button onclick={menos}>menos</button>
-    </>
-  );
+
+    
+      
+  
+
 }
 
